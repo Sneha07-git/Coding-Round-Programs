fa8d30b61231d27e87e272def355088441f62743
Sneha Chindarkar <67257891+Sneha07-git@users.noreply.github.com>
2021-03-12 15:06:26 +0530

    Java Pattern- 12345 1234 123...

---
+++ Number_pattern.java
@@ -0,0 +1,22 @@
+package pattern_programs;
+
+public class Number_pattern {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=5; i>=1; i--) {
+			
+			//System.out.print(i);
+			
+			for(int j=1; j<=i; j++) {
+				
+				System.out.print(j);
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
